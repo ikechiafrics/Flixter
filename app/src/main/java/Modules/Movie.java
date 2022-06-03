4abe0fd61fe50bd4e507eb273af5a52ccf399f65
ikechiafrics <93938295+ikechiafrics@users.noreply.github.com>
2022-06-02 22:27:31 -0700

    first commit

---
+++ app/src/main/java/Modules/Movie.java
@@ -0,0 +1,58 @@
+package Modules;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Movie {
+
+    String posterPath;
+    String title;
+    String overview;
+    String backdropPath;
+    Double voteAverage;
+
+    public Movie(){};
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        backdropPath = jsonObject.getString("backdrop_path");
+        voteAverage = jsonObject.getDouble("vote_average");
+    }
+
+
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w500/%s", posterPath);
+    }
+
+    public String getBackdropPath(){
+        return String.format("https://image.tmdb.org/t/p/w500/%s", backdropPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
---
+++ app/src/main/java/adapters/MovieAdapter.java
@@ -0,0 +1,104 @@
+package adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixterproject.MovieDetailsActivity;
+import com.example.flixterproject.R;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import Modules.Movie;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter","onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie,parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter","onBindViewHolder" + position);
+        Movie movie = movies.get(position);
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+                Glide.with(context).load(imageUrl).placeholder(R.drawable.backdrop_placeholder).into(ivPoster);
+            } else {
+                imageUrl = movie.getPosterPath();
+                Glide.with(context).load(imageUrl).placeholder(R.drawable.movie_placeholder).into(ivPoster);
+            }
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+
+            if (position != RecyclerView.NO_POSITION){
+                Movie movie = movies.get(position);
+
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+
+                context.startActivity(intent);
+            }
+        }
+    }
+}
